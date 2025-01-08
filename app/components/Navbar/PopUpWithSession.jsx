99206e10bff20126ba365053d0b7dde1babde972
ashik-coder <ashikhasan998@gmail.com>
2025-01-08 23:59:00 +0600

    uplode

--- app/components/Navbar/PopUpWithSession.jsx
+++ app/components/Navbar/PopUpWithSession.jsx
@@ -39,7 +39,7 @@ const PopUpWithSession = () => {
 
                 <li
                     onClick={handelLogout}
-                    className="px-3 py-2 text-sm text-zinc-700 transition-all hover:bg-zinc-50 hover:text-zinc-800 hover:pl-4">
+                    className="px-3 py-2 cursor-pointer text-sm text-zinc-700 transition-all hover:bg-zinc-50 hover:text-zinc-800 hover:pl-4">
                     Log Out
                 </li>
 
